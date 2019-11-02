f2c156e03c4337e4e9fdf475c767f6f99a352894
sjconst <stephanie.c.lake@gmail.com>
2019-11-02 13:02:46 -0700

    config config files and index.js under models

---
+++ config/config.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const config = {
+  development: {
+    username: "root",
+    password: process.env.DB_PASS,
+    database: "exampledb",
+    host: "localhost",
+    dialect: "mysql"
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "testdb",
+    host: "localhost",
+    dialect: "mysql",
+    logging: false
+  },
+  production: {
+    use_env_variable: "JAWSDB_URL",
+    dialect: "mysql"
+  }
+}
+module.exports = config;
