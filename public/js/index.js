e2e43d92ff98e0a4b93e8f5c8e210df993da3a5e
sjconst <stephanie.c.lake@gmail.com>
2019-11-06 09:59:05 -0800

    api calls added, edits to routes

--- public/js/index.js
+++ public/js/index.js
@@ -1,88 +1,67 @@
 // Get references to page elements
-var $exampleText = $("#example-text");
-var $exampleDescription = $("#example-description");
-var $submitBtn = $("#submit");
-var $exampleList = $("#example-list");
+const $submitBtn = $("#submit");
+const $name = $("#name");
+const $email = $("#email");
+const $energy = $("#energy-select"); 
+const $information =  $("#information-select");
+const $decisions = $("#decisions-select");
+const $organization = $("#organization-select");
+//BUG: can't get it to read the process.env here;
+//const token = process.env.Crystal_Token;
+const token = "d18a5972dc9f0d4460748e825941f8c6"
 
-// The API object contains methods for each kind of request we'll make
-var API = {
-  saveExample: function(example) {
+const API = {
+  saveTest: result => {
     return $.ajax({
       headers: {
         "Content-Type": "application/json"
       },
       type: "POST",
-      url: "api/examples",
-      data: JSON.stringify(example)
+      url: "api/tests",
+      data: JSON.stringify(result)
     });
   },
-  getExamples: function() {
+  getTests: () => {
     return $.ajax({
-      url: "api/examples",
+      url: "api/tests",
       type: "GET"
     });
   },
-  deleteExample: function(id) {
+  getByType: (letter1, letter2, letter3, letter4) => {
     return $.ajax({
-      url: "api/examples/" + id,
+      url: `api/type/${letter1}${letter2}${letter3}${letter4}`,
+      type: "GET"
+    });
+  },
+  deleteTest: id => {
+    return $.ajax({
+      url: `api/tests/${id}`,
       type: "DELETE"
     });
+  },
+  personalityTest: (name, email) => {
+    return $.ajax({
+      url: `https://app.crystalknows.com/personality-test?api_customer_id=${token}&api_company_name=${name}&api_user_email=${email}`,
+      type: "GET"
+    })
   }
 };
 
-// refreshExamples gets new examples from the db and repopulates the list
-var refreshExamples = function() {
-  API.getExamples().then(function(data) {
-    var $examples = data.map(function(example) {
-      var $a = $("<a>")
-        .text(example.text)
-        .attr("href", "/example/" + example.id);
-
-      var $li = $("<li>")
-        .attr({
-          class: "list-group-item",
-          "data-id": example.id
-        })
-        .append($a);
-
-      var $button = $("<button>")
-        .addClass("btn btn-danger float-right delete")
-        .text("ｘ");
-
-      $li.append($button);
-
-      return $li;
-    });
-
-    $exampleList.empty();
-    $exampleList.append($examples);
-  });
-};
-
-// handleFormSubmit is called whenever we submit a new example
-// Save the new example to the db and refresh the list
-var handleFormSubmit = function(event) {
+// handleFormSubmit
+// Get name and email from form and send to personality test API
+const handleFormSubmit = function(event) {
   event.preventDefault();
-
-  var example = {
-    text: $exampleText.val().trim(),
-    description: $exampleDescription.val().trim()
-  };
-
-  if (!(example.text && example.description)) {
+  let name = $name.val().trim();
+  let email = $email.val().trim();
+  if (!(name && email)) {
     alert("You must enter an example text and description!");
     return;
   }
-
-  API.saveExample(example).then(function() {
-    refreshExamples();
-  });
-
-  $exampleText.val("");
-  $exampleDescription.val("");
+  let data = API.personalityTest(name, email);
+  console.log(data);  
+  API.saveTest(data);
 };
 
-// handleDeleteBtnClick is called when an example's delete button is clicked
 // Remove the example from the db and refresh the list
 var handleDeleteBtnClick = function() {
   var idToDelete = $(this)
@@ -93,17 +72,14 @@ var handleDeleteBtnClick = function() {
     refreshExamples();
   });
 };
-
 // Add event listeners to the submit and delete buttons
 $submitBtn.on("click", handleFormSubmit);
-$exampleList.on("click", ".delete", handleDeleteBtnClick);
-
+// $exampleList.on("click", ".delete", handleDeleteBtnClick);
 // Dropdown Trigger 
-$(".dropdown-trigger").dropdown({
-  coverTrigger: false,
-  hover: true
-});
-
+// $(".dropdown-trigger").dropdown({
+//   coverTrigger: false,
+//   hover: true
+// });
 // Form Select
 $(document).ready(function(){
   $('select').formSelect();
