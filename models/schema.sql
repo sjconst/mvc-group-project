cb555b97d08848eaef0280d7423e0d9a78822094
redricasa <fredeb26@gmail.com>
2019-11-05 21:43:15 -0800

    corrected a typo in readme. deleted node_modules and package.json and re installed npm's. added a new sql schema to match the sequelize model. debugged example.js to create the sequelize model.

--- models/schema.sql
+++ models/schema.sql
@@ -3,3 +3,17 @@ CREATE DATABASE personality_test;
 
 DROP DATABASE IF EXISTS testdb;
 CREATE DATABASE testdb;
+
+-- 
+USE personality_test;
+
+CREATE TABLE SurveyResults (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    name_ VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    test_type VARCHAR(255) NOT NULL,
+    results VARCHAR(255) NOT NULL,
+    createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
+-- 
