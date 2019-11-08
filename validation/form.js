a98161e381fa07e894bae9d58a5acb3ca255190e
redricasa <fredeb26@gmail.com>
2019-11-07 20:20:14 -0800

    added a comment in form.js since we can use it for front end validation.

--- validation/form.js
+++ validation/form.js
@@ -1,3 +1,4 @@
+//this can be used to test front-end without the module export line
 const checkEmpty = function(form){
     if (form.value == null || form.value == undefined || form.value.length == 0){
         alert("name can't be empty");
